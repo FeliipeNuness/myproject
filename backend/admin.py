dfdebf073ab25ef5c697514521139df3d98c0b72
Felipe <felipe.dev0809@gmail.com>
2024-12-06 06:55:23 -0300

    mudanças no admin e models

--- backend/admin.py
+++ backend/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import Category, Product, Transactions, Plan, Signature
+from backend.models import Category, Product, Transactions, Plan, Signature
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
-    search_fields = ('name',)
-    list_per_page = 25
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'stock', 'category', 'user')
     
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('TIPOS', 'product', 'type', 'amount', 'date', 'user')
+    
+
+@admin.register(Plan)        
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'monthly_price', 'description', 'active')
+    
+
+@admin.register(Signature)    
+class SignatureAdmin(admin.ModelAdmin):
+    list_display = ('user', 'flat', 'start_date', 'end_date')
--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    descripition = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveBigIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product")
